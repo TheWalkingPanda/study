398bb00e3844854da09471c02675670ec84ae78b
2770078682@qq.com <2770078682@qq.com>
2017-05-13 16:20:44 +0800

    init

---
+++ groovy/src/com/study/groovy/Groovy01_HelloWorld.groovy
@@ -0,0 +1,10 @@
+package com.study.groovy
+
+/**
+ * Created by Administrator on 2017/5/13.
+ */
+class Groovy01_HelloWorld {
+    static void main(String[] args) {
+        println "hello world"
+    }
+}
